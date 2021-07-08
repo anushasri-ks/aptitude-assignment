82aaf1cd476c3d27e9350184df2b57f3d348aafc
Anushasri KS <83582662+anushasri-ks@users.noreply.github.com>
2021-07-08 22:39:51 +0530

    different pattern form

---
+++ RightsideUpBorderTriangle.java
@@ -0,0 +1,20 @@
+package com.xworkz.aptitudeclass.assignment2;
+
+import java.util.Scanner;
+
+public class RightsideUpBorderTriangle {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter square matrix limit: ");
+		int rc = sc.nextInt();
+
+		for (int r = 1; r <= rc; r++) {
+			for (int c = 1; c <= rc; c++) {
+				if (r == c || r == 1 || c == 5)
+					System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
