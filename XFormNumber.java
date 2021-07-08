4798e7f08c8a2ab693aad9d4f0f56fc3b79f68f1
Anushasri KS <83582662+anushasri-ks@users.noreply.github.com>
2021-07-08 20:06:30 +0530

    Update XFormNumber.java

--- XFormNumber.java
+++ XFormNumber.java
@@ -4,29 +4,33 @@ import java.util.Scanner;
 
 public class XFormNumber {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter square matrix limit: ");
-		int rc = sc.nextInt();
-		int mid = (rc / 2) + 1;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter square matrix limit: ");
+	int rc = sc.nextInt();
+	int mid = (rc / 2) + 1;
 
-		for (int r = 1; r <= rc; r++) {
-			for (int c = 1; c <= rc; c++) {
-				if (c >= mid && r + c == rc + 1)
-					System.out.print(c);
-				else if (c < mid && r + c == rc + 1) {
-					System.out.print(r);
-				} else if (r == c && c <= mid) {
-					System.out.print(c);
-				} else if (r == c && c > mid) {
-					if (c != 0)
-						mid -= 1;
-					System.out.print(mid);
-				} else
-					System.out.print(" ");
-			}
-			System.out.println();
+	for (int r = 1; r <= rc; r++) {
+	    for (int c = 1; c <= rc; c++) {
+	        if (r + c == rc + 1) {
+		    if (c >= mid)
+		        System.out.print(c);
+		    else
+			System.out.print(r);
+		} else if (r == c) {
+		    if (c < mid) {
+			System.out.print(c);
+		    } else if (c >= mid && rc % 2 == 0) {
+			mid -= 1;
+	                System.out.print(mid);
+		    } else {
+			mid -= 1;
+		        System.out.print(mid);
+		} else
+		    System.out.print(" ");
 		}
-	}
+            System.out.println();
+        }
+    }
 }
