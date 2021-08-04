94776c87738428bb253f2ffd0b78e8d3ae623b0f
Anushasri KS <83582662+anushasri-ks@users.noreply.github.com>
2021-08-04 16:47:01 +0530

    test

---
+++ PatternTest.java
@@ -0,0 +1,14 @@
+package com.xworkz.aptitudeclass.test;
+
+import java.util.Scanner;
+
+public class PatternTest {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int row = sc.nextInt();
+		
+		System.out.println("a");
+	}
+}
