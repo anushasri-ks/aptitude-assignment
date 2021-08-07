cf6284e92d2d4645595d20be432869f11f99c139
Anushasri KS <83582662+anushasri-ks@users.noreply.github.com>
2021-08-07 15:48:50 +0530

    Apti pattern test

--- PatternTest.java
+++ PatternTest.java
@@ -1,14 +1,108 @@
 package com.xworkz.aptitudeclass.test;
 
-import java.util.Scanner;
-
 public class PatternTest {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int row = sc.nextInt();
-		
-		System.out.println("a");
+
+		int row = 7;
+		int mid = row / 2 + 1;
+		PatternK(row, mid);
+		System.out.println();
+		patternNum(row);
+		System.out.println();
+		oddEvenPosition(row);
+		System.out.println();
+		patternV(row, mid);
+		System.out.println();
+		patternSpaced(row);
+		System.out.println();
+		patternDiamond(row, mid);
+		System.out.println();
+	}
+
+	public static void patternDiamond(int row, int mid) {
+		for (int r = 1; r <= row; r++) {
+			for (int c = 1; c <= row; c++) {
+				if (r <= mid && r + c >= mid + 1 && c - r < mid)
+					System.out.print("* ");
+				else if (r > mid && r - c < mid && r + c <= row + mid)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void patternSpaced(int row) {
+		for (int r = 1; r <= row; r++) {
+			for (int c = 1; c <= row; c++) {
+				if (r % 2 == 0 && c % 2 == 0)
+					System.out.print("* ");
+				else if (r % 2 != 0 && c % 2 != 0)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void patternV(int row, int mid) {
+		for (int r = 1; r <= row; r++) {
+			for (int c = 1; c <= row; c++) {
+				if (r == row && c == mid)
+					System.out.print("**");
+				else if (r <= mid && (c == 1 || c == row))
+					System.out.print("**");
+				else if (c + r == row + mid)
+					System.out.print("**");
+				else if (r - c == row / 2)
+					System.out.print("**");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void oddEvenPosition(int row) {
+		for (int r = 1; r <= row; r++) {
+			for (int c = 1; c <= row; c++) {
+				if (r % 2 == 0 && c % 2 != 0)
+					System.out.print(r + "" + c);
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void patternNum(int row) {
+		for (int r = 1; r <= row; r++) {
+			for (int c = 1; c <= row; c++) {
+				if (c == 1)
+					System.out.print(r + " ");
+				else if (r >= c)
+					System.out.print(c + r - 1 + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void PatternK(int row, int mid) {
+		for (int r = 1; r <= row; r++) {
+			for (int c = 1; c <= row; c++) {
+				if (c == 1)
+					System.out.print("**");
+				else if (r + c == row + 1 && r <= mid)
+					System.out.print("**");
+				else if (r == c && r > mid)
+					System.out.print("**");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
 	}
 }
